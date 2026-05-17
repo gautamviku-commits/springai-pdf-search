eb65441cf7ebe2170e092454910d8dfefbd13ff9
gautamviku-commits <gautam.viku@gmail.com>
2026-05-17 11:28:01 +0530

    Add main Spring Boot application class

---
+++ src/main/java/com/example/springai/pdf/SpringAiPdfSearchApplication.java
@@ -0,0 +1,12 @@
+package com.example.springai.pdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiPdfSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiPdfSearchApplication.class, args);
+    }
+}
